dbae14c708515c3d6d3176045d30a475d2695037
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2023-04-04 20:08:32 +0200

    Rozwiązane

--- Semestr2_Lekcja5_2023/Program.cs
+++ Semestr2_Lekcja5_2023/Program.cs
@@ -14,16 +14,59 @@ namespace Semestr2_Lekcja5_2023
 
             GenereujPostaci generator = new GenereujPostaci();
 
-            Mag mag = generator.GenerujMaga();
+            Mag gracz = generator.GenerujMaga();
+            Wojownik przeciwnik = generator.GenerujWojownika();
 
-            Wojownik woj = generator.GenerujWojownika();
+            Console.WriteLine("Na swojej drodze spotkałeś przeciwnika.");
+            przeciwnik.WyswietlStatystyki();
 
+            Console.WriteLine("Czy chcesz zaatakować (wpisz: atak) czy uciekać (wpisz: ucieczka)");
+            string odp = Console.ReadLine();
 
-            mag.WyswietlStatystyki();
-            woj.WyswietlStatystyki();
+            if (odp != "atak")
+            {
+                Console.WriteLine("Uciekasz....");
 
-            //do zadania 12
-            Console.ReadLine();
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Do ataku !!!");
+            // walka toczy się dopóki życie któregoś z graczy nie spadnie poniżej zera
+            while (gracz.Hp >= 0 && przeciwnik.Hp >= 0)
+            {
+                // przeciwnik atakuje
+                Console.WriteLine($"Gracz {przeciwnik.Nazwa} atakuje.");
+                int atakPrzeciwnika = przeciwnik.PobierzSileAtaku();
+                gracz.OdejmijHp(atakPrzeciwnika);
+                Console.WriteLine($"Pozostało Ci {gracz.Hp} życia.\n");
+                // sprawdzamy czy po ataku czasem nie zginęliśmy
+                // jeżeli tak to kończymy walkę
+                if (gracz.Hp <= 0)
+                    break;
+                // zatrzymujemy na pół sekundy program dla ciekawszego efektu
+                System.Threading.Thread.Sleep(500);
+                // my atakujemy
+                Console.WriteLine($"Gracz {gracz.Nazwa} atakuje.");
+                int atakGracza = gracz.PobierzSileAtaku();
+                przeciwnik.OdejmijHp(atakGracza);
+                Console.WriteLine($"Przeciwnikowi zostało {przeciwnik.Hp} życia.\n");
+                // sprawdzamy czy po ataku czasem nie pokonaliśmy wroga
+                // jeżeli tak to kończymy walkę
+                if (przeciwnik.Hp <= 0)
+                    break;
+                // zatrzymujemy na pół sekundy program dla ciekawszego efektu
+                System.Threading.Thread.Sleep(500);
+            }
+            if (gracz.Hp <= 0)
+            {
+                Console.WriteLine($"Niestety zostałeś pokonany. Przeciwnik {przeciwnik.Nazwa} wygrał.");
+            }
+            else
+            {
+                Console.WriteLine($"Gratulację. Pokonałeś przeciwnika {przeciwnik.Nazwa}");
+            }
+            Console.ReadKey();
         }
     }
 }
